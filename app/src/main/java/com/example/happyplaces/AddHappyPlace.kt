215e39f78e88037734381da4ddb41a8235c59082
albertohuerta <albertohuerta22@gmail.com>
2022-06-28 00:11:22 +0100

    Added back button from AddHappy > Main

--- app/src/main/java/com/example/happyplaces/AddHappyPlace.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyPlace.kt
@@ -2,10 +2,18 @@ package com.example.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_happy_place.*
 
 class AddHappyPlace : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+
+        //add back button
+        setSupportActionBar(toolbar_add_place)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar_add_place.setNavigationOnClickListener{
+            onBackPressed()
+        }
     }
 }
