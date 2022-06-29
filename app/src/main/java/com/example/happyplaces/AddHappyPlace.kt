9eac08e870cb4b70808e56dfbbd76c24e68e0f79
albertohuerta <albertohuerta22@gmail.com>
2022-06-29 23:55:20 +0100

    Bug introduced for date picker,
    dateSetListener func not working; possible import error

--- app/src/main/java/com/example/happyplaces/AddHappyPlace.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyPlace.kt
@@ -1,10 +1,18 @@
 package com.example.happyplaces
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_add_happy_place.*
+import java.util.*
 
 class AddHappyPlace : AppCompatActivity() {
+
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
@@ -16,4 +24,9 @@ class AddHappyPlace : AppCompatActivity() {
             onBackPressed()
         }
     }
+
+
+    dateSetListener = DatePickerDialog.OnDateSetListener{
+        view, year, month, dayOfMonth -> }
+
 }
